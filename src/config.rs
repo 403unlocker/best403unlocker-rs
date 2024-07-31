0ac155e9cde39f8c64ee99beb7b099616a9bba85
Pourya <prbarkati@gmail.com>
2024-07-31 15:58:40 +0330

    change `confi.rs` file to read all possible location of `best403unlocker.conf` file.

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,6 @@
 use serde::Deserialize;
-use std::fs;
-use std::path::Path;
+use std::{env, fs, process};
+use std::path::{Path, PathBuf};
 
 #[derive(Deserialize)]
 pub struct Config {
@@ -11,19 +11,46 @@ pub struct Config {
 
 impl Config {
     pub fn load() -> Result<Self, Box<dyn std::error::Error>> {
-        let config_path = Path::new("best403unlocker.conf");
+        //check different location for .conf file existence
+        let config_path = Path::new("../best403unlocker.conf");
         let config_content = if config_path.exists() {
             fs::read_to_string(config_path)?
-        } else {
-            let etc_config_path = Path::new("/etc/best403unlocker.conf");
-            if etc_config_path.exists() {
-                fs::read_to_string(etc_config_path)?
-            } else {
-                todo!("Implement downloading the config file if not found");
-                // TODO: Implement downloading the config file if not found
-                return Err("Configuration file not found".into());
-            }
-        };
+        }
+            else {
+               let config_path =if cfg!(target_os="windows"){
+
+                   let home_dir = env::var("USERPROFILE").expect("Couldn't read home directory path");
+                   let config_path = Path::new("AppData/Roaming/best403unlocker/best403unlocker.conf");
+                   PathBuf::from(&home_dir).join(&config_path)
+
+               }
+                else{
+                    let home_dir = env::var("HOME").expect("Couldn't read home directory path");
+                    let config_path = Path::new(".config/best403unlocker/best403unlocker.conf");
+                    PathBuf::from(&home_dir).join(&config_path)
+                };
+                if config_path.exists(){
+
+                    fs::read_to_string(&config_path)?
+                }
+
+                else{
+                    if cfg!(target_os = "windows"){
+                    eprintln!("failed to find `best403unlocker.conf` file");
+                        process::exit(1)
+                    }
+                    let etc_config_path = Path::new("/etc/best403unlocker.conf");
+                    if etc_config_path.exists() {
+                        fs::read_to_string(etc_config_path)?
+                    } else {
+                        eprintln!("failed to find `best403unlocker.conf` file");
+                        process::exit(1);
+                        todo!("Implement downloading the config file if not found");
+                        // TODO: Implement downloading the config file if not found
+                        return Err("Configuration file not found".into());
+                    }
+                }
+            };
         let mut config: Config = toml::from_str(&config_content)?;
         config.dns = config.dns.iter().map(|s| s.trim().to_string()).collect();
         Ok(config)
