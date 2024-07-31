7413d5a016c188ca47d8a396893c857b205a2297
Pourya <prbarkati@gmail.com>
2024-07-31 14:41:55 +0330

    create a config directory in the `~/.config` path for the project

--- build.rs
+++ build.rs
@@ -3,10 +3,10 @@ use std::fs;
 use std::path::PathBuf;
 
 #[cfg(target_os = "windows")]
-const BIN_DIR: &str = ".cargo/bin";
+const CONFIG_DIR: &str = "AppData/Roaming/best403unlocker";
 
 #[cfg(not(target_os = "windows"))]
-const BIN_DIR: &str = ".cargo/bin";
+const CONFIG_DIR: &str = ".config/best403unlocker";
 
 fn main() {
     // Path to the configuration file
@@ -14,17 +14,20 @@ fn main() {
 
     // Home directory of the current user
     let home_dir = if cfg!(target_os = "windows") {
-        env::var("USERPROFILE").expect("Couldn't read home directory path ")
+        env::var("USERPROFILE").expect("Couldn't read home directory path")
     } else {
-        env::var("HOME").expect("Couldn't read home directory path ")
+        env::var("HOME").expect("Couldn't read home directory path")
     };
 
-    // Path to the ~/.cargo/bin/ directory
-    let cargo_bin_dir = PathBuf::from(home_dir).join(BIN_DIR);
+    // Path to the config directory
+    let config_dir = PathBuf::from(&home_dir).join(CONFIG_DIR);
 
-    // Copy the configuration file to the ~/.cargo/bin/ directory
-    let dest_path = cargo_bin_dir.join(conf_file);
-    println!("{:?}",dest_path);
+    // Create the config directory if it doesn't exist
+    fs::create_dir_all(&config_dir).expect("Failed to create config directory");
+
+    // Copy the configuration file to the config directory
+    let dest_path = config_dir.join(conf_file);
+    println!("Copying configuration file to: {:?}", dest_path);
     fs::copy(conf_file, &dest_path).expect("Failed to copy configuration file");
 
     println!("cargo:rerun-if-changed={}", conf_file);
